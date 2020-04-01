4aa4a299a8cd3eaef1d21e3ad3821bfc1d6da33f
EliFish <elijahleis10@gmail.com>
2020-04-01 12:08:25 +0800

    UPDATE1

--- dashboardAtmAndCam/src/main/java/dashboard/atmandcam/MyMain.java
+++ dashboardAtmAndCam/src/main/java/dashboard/atmandcam/MyMain.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		SpringApplication.run(MyMain.class, args);
 
 	}
