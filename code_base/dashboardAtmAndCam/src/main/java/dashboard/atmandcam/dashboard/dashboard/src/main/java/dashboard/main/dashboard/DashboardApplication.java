f46e0b55c81d80898d585994b2a3b2d7f481e2ad
draceynd <62893000+draceynd@users.noreply.github.com>
2020-04-03 14:28:55 +0800

    Add files via upload

---
+++ code_base/dashboardAtmAndCam/src/main/java/dashboard/atmandcam/dashboard/dashboard/src/main/java/dashboard/main/dashboard/DashboardApplication.java
@@ -0,0 +1,22 @@
+package dashboard.main.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("dashboard.main")
+@EntityScan("dashboard.main.Model")
+@EnableJpaRepositories("dashboard.main.Repository")
+public class DashboardApplication {
+		
+		public static void main(String[] args) {
+			SpringApplication.run(DashboardApplication.class, args);
+			System.out.println("Hello");
+			
+			
+		}
+		
+	}
---
+++ code_base/dashboardAtmAndCam/src/main/java/dashboard/atmandcam/dashboard/dashboard/target/classes/static/js/demo.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').fadeOut();
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').fadeIn();
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+
+      $(function () {
+         $('.tabs').click(function () {
+            $('.tabme').not('#div' + $(this).attr('target')).hide();
+            $('#div' + $(this).attr('target')).show();
+         });
+      }); 
+
+
+      
