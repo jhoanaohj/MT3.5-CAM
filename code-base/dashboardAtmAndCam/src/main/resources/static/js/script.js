4deaf876133a86c0c908970e8032faf5e8e3e39f
EliFish <elijahleis10@gmail.com>
2020-04-13 21:46:30 +0800

    ELeis - UPDATE of output-screenshot (uploading of screenshot of dashboard)

--- code-base/dashboardAtmAndCam/src/main/resources/static/js/script.js
+++ code-base/dashboardAtmAndCam/src/main/resources/static/js/script.js
@@ -5,69 +5,20 @@ var staticUrl = "/queries/31370100";
     console.log(data[0].operation_start);
 });
 
-// $(document).ready(function () {
-//     $('#sidebarCollapse').on('click', function () {
-//         $('#sidebar').toggleClass('active');
-// });
-        
-// });
-
-//var jsonData2 = '[{"id":"31370100","machine_name":"GUMACA BRANCH CAM","machine_type":"CAM","operation_start":"05:04:33","operation_end":"09:04:33"}]'
-
-// d3.json(url).then(function (data) {
-
-//   if (typeof data === 'undefined' || data === null) {
-//       console.error("Failed to retreive the data");
-//   } else {
-//       console.log(data);
-//       window.alert(data.machine_name);
-//   }
-// }).catch(function (error) {
-//   console.error("Failed to retreive the data or there was an error parsing the response: " + error);
-// });
-
-// var obj = JSON.parse(jsonData);
-// document.getElementById("demo").innerHTML = obj.id + obj.machine_name;
-
-// var	margin = {top: 30, right: 20, bottom: 30, left: 50},
-// 	width = 500 - margin.left - margin.right,
-// 	height = 50 - margin.top - margin.bottom;
-
-// var dataset = [1,2,3,4,5,100,39,49,12,1.1,31,40];
-// var svgWidth = 500, svgHeight = 300, barPadding = 10;
-// var barWidth = (width / dataset.length);
-//window.alert(barWidth)
-// var svg = d3.select('svg')
-// .attr("width", svgWidth)
-// .attr("height", svgHeight);
-
-// var yScale = d3.scaleLinear()
-// .domain([0, d3.max(dataset)])
-// .range([0,svgHeight]);
-
-// var xScale = d3.scaleLinear()
-// .domain([d3.max(dataset),0])
-// .range(([svgWidth, 0]));
-
-// var barChart = svg.selectAll("rect")
-// .data(dataset)
-// .enter()
-// .append("rect")
-// .attr("y", function(d){
-//     return svgHeight - yScale(d);
-// })
-// .attr("x",(d)=>{
-//     return 85;
-// })
-// .attr("height", d => yScale(d))
-// .attr("width", barWidth - barPadding)
-// .attr("transform", function(d,i){
-//     var translate = [barWidth * i, 0];
-//     console.log("translate " + translate);
-//     console.log("distance to each other: " + (barWidth - barPadding));
-//     console.log(i)
-//     return "translate(" + translate + ")";
-// })
+var staticUrlTop5Downtime = "/top5downtime/";
+ var jsonData = $(document).ready(() => {
+    $.ajax({
+        url: (staticUrlTop5Downtime),
+        method: "GET",
+        dataType: "json",
+        success: (data) => {
+            $.each(data, (key, val) => {
+                $("#top5Downtime").append("<li>" + val.event_description + " - " + val.error_count + "</li>");
+            });
+        }
+    })
+});
+ 
 const sample = [
     {
       language: 'Rust',
@@ -136,18 +87,18 @@ const sample = [
       }
   ];
 
-  const svg = d3.select('svg')
-  .attr("width", "100%")
-  .attr("height", "100%");
+  //const svg = d3.select('svg');
   
-  const svgContainer = d3.select('#container');
+  const svgContainer = d3.select('#bar-chart')
+  .append("svg");
   
   const margin = 80;
+  const svgWidth = 50;
   const width = 1100 - 2 * margin;
   const height = 600 - 2 * margin;
 
-  const chart = svg.append('g')
-    .attr('transform', "translate(" + margin + ","+ margin +")");
+  const chart = svgContainer.append('g')
+    .attr('transform', "translate(" + svgWidth + ","+ margin +")");
 
   const xScale = d3.scaleBand()
     .range([0, width])
@@ -251,36 +202,26 @@ const sample = [
     .attr('text-anchor', 'middle')
     .text((a) => `${a.value}%`)
   
-  svg
+    svgContainer
     .append('text')
     .attr('class', 'label')
     .attr('x', -(height / 2) - margin)
-    .attr('y', margin / 2.4)
+    .attr('y', svgWidth / 2.4)
     .attr('transform', 'rotate(-90)')
     .attr('text-anchor', 'middle')
     .text('Love meter (%)')
 
-  svg.append('text')
+    svgContainer.append('text')
     .attr('class', 'label')
     .attr('x', width / 2 + margin)
     .attr('y', height + margin * 1.7)
     .attr('text-anchor', 'middle')
     .text('Languages')
 
-  svg.append('text')
+    svgContainer.append('text')
     .attr('class', 'title')
     .attr('x', width / 2 + margin)
     .attr('y', 40)
     .attr('text-anchor', 'middle')
-    .text('Most loved programming languages in 2018')
+    .text('Most loved programming languages in 2018');
 
-  svg.append('text')
-    .attr('class', 'source')
-    .attr('x', width - margin / 2)
-    .attr('y', height + margin * 1.7)
-    .attr('text-anchor', 'start')
-    .text('Source: Stack Overflow, 2018')
-
-
-
- 
---
+++ code-base/dashboardAtmAndCam/target/classes/static/js/pieChart.js
@@ -0,0 +1,46 @@
+var data = [
+    {"platform": "Android", "percentage": 40.11},
+    {"platform": "Windows", "percentage": 36.69},
+    {"platform": "iOS", "percentage": 13.06}
+];
+
+
+var svgCirWidth = 600, svgCirHeight = 300, radius = Math.min(svgCirWidth, svgCirHeight) / 2;
+
+const pieContainer = d3.select("#pieChart")
+.append("svg")
+.attr("width", svgCirWidth)
+.attr("height", svgCirHeight);
+
+//create group element to hold pie chart
+
+var g = pieContainer.append("g")
+.attr("transform", "translate(" + 170 + "," + radius + ")");
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var pie = d3.pie().value(function(d){
+    return d.percentage;
+});
+
+var path = d3.arc()
+.outerRadius(radius)
+.innerRadius(0);
+
+var arc = g.selectAll("arc")
+.data(pie(data))
+.enter() //means keeps looping in the data
+.append("g");
+
+arc.append("path")
+.attr("d", path)
+.attr("fill", function(d){
+    return color(d.data.percentage);
+});
+
+var label = d3.arc()
+.outerRadius(radius)
+.innerRadius(0)
+.text(function(d){
+    return d;
+});
